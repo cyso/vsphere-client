068a1bae62c4a6013e5719366e0fa4f37bd550ba
Nick Douma <n.douma@nekoconeko.nl>
2013-11-18 15:03:34 +0100

    Bump version, update changelog

--- src/nl/cyso/vsphere/client/config/Version.java
+++ src/nl/cyso/vsphere/client/config/Version.java
@@ -26,11 +26,11 @@ public class Version extends nl.nekoconeko.configmode.Version {
 	/**
 	 * Official release version of vsphere-client
 	 */
-	public final String RELEASE_VERSION = "0.4";
+	public final String RELEASE_VERSION = "0.4.1";
 	/**
 	 * Actual Git revision according to `git describe` during bumping
 	 */
-	public final String BUILD_VERSION = "3a59a82";
+	public final String BUILD_VERSION = "5193baf";
 
 	private static Version instance = null;
 
